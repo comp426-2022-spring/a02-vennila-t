59d3b36d268e21d4e43110b8abe01b357bed3de2
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-02-07 21:38:26 -0500

    update

--- flip.js
+++ flip.js
@@ -1,3 +1,7 @@
 // Import the coinFlip function from your coin.mjs file
-
+function coinFlip() {
+    var coin = ['heads', 'tails'];
+    return coin[Math.floor(Math.random()*coin.length)];
+}
 // Call the coinFlip function and put the return into STDOUT
+console.log(coinFlip())
