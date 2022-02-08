be54f08cf55bbc600708923d52056438e4c5f7f8
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-02-07 21:42:00 -0500

    update

--- flip.js
+++ flip.js
@@ -1,7 +1,4 @@
 // Import the coinFlip function from your coin.mjs file
-function coinFlip() {
-    var coin = ['heads', 'tails'];
-    return coin[Math.floor(Math.random()*coin.length)];
-}
+import function coinFlip(){}
 // Call the coinFlip function and put the return into STDOUT
 console.log(coinFlip())
--- modules/coin.mjs
+++ modules/coin.mjs
@@ -1,31 +1,32 @@
-/** Coin flip functions 
+/** Coin flip functions
  * This module will emulate a coin flip given various conditions as parameters as defined below
  */
 
 /** Simple coin flip
- * 
+ *
  * Write a function that accepts no parameters but returns either heads or tails at random.
- * 
+ *
  * @param {*}
- * @returns {string} 
- * 
+ * @returns {string}
+ *
  * example: coinFlip()
  * returns: heads
- * 
+ *
  */
 
 function coinFlip() {
-
+  var coin = ['heads', 'tails'];
+  return coin[Math.floor(Math.random()*coin.length)];
 }
 
 /** Multiple coin flips
- * 
- * Write a function that accepts one parameter (number of flips) and returns an array of 
+ *
+ * Write a function that accepts one parameter (number of flips) and returns an array of
  * resulting "heads" or "tails".
- * 
- * @param {number} flips 
+ *
+ * @param {number} flips
  * @returns {string[]} results
- * 
+ *
  * example: coinFlips(10)
  * returns:
  *  [
@@ -42,15 +43,15 @@ function coinFlips(flips) {
 }
 
 /** Count multiple flips
- * 
- * Write a function that accepts an array consisting of "heads" or "tails" 
- * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ *
+ * Write a function that accepts an array consisting of "heads" or "tails"
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning
  * an object containing the number of each.
- * 
+ *
  * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
  * { tails: 5, heads: 5 }
- * 
- * @param {string[]} array 
+ *
+ * @param {string[]} array
  * @returns {{ heads: number, tails: number }}
  */
 
@@ -59,12 +60,12 @@ function countFlips(array) {
 }
 
 /** Flip a coin!
- * 
- * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
- * 
- * @param {string} call 
+ *
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose".
+ *
+ * @param {string} call
  * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
- * 
+ *
  * example: flipACoin('tails')
  * returns: { call: 'tails', flip: 'heads', result: 'lose' }
  */
@@ -74,7 +75,8 @@ function flipACoin(call) {
 }
 
 
-/** Export 
- * 
+/** Export
+ *
  * Export all of your named functions
 */
+export function coinFlip(){}
