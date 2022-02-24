5817872029edadeb380d969bc2432cc7a2554749
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-02-23 22:10:46 -0500

    update

--- flips.js
+++ flips.js
@@ -1,7 +1,10 @@
 // Import the coinFlip function from your coin.mjs file
 import {coinFlips} from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-const args = require("yargs");
+const args = require('yargs');
+
 
 // Define allowed argument name 'number'.
 
