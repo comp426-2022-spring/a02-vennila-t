2755675a2b263117f37ee0768febecdebe2dd6fc
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-02-23 21:47:39 -0500

    update

--- flips.js
+++ flips.js
@@ -1,6 +1,7 @@
 // Import the coinFlip function from your coin.mjs file
 import {coinFlips} from "./modules/coin.mjs";
-const args = require('minimist')(process.argv.slice(2))
+import minimist from 'cjs';
+minimist = (process.argv.slice(2));
 // Define allowed argument name 'number'.
 args["number"]
 // Call the coinFlip function and put the return into STDOUT
