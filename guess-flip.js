c14a319abf3ead05df2bf5d230c5ad573d7e2235
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-03-22 15:58:09 -0400

    update

--- guess-flip.js
+++ guess-flip.js
@@ -1,17 +1,25 @@
 // Import the coinFlip and flipACoin function from your coin.mjs file
-import { flipACoin } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
+const args = require('yargs')(process.argv.slice(2));
 const args = require('yargs').argv;
 
 
 // Define allowed argument name 'call'.
 
+// Call the coinFlip function and put the return into STDOUT
+
+// Call the coinFlip function and put the return into STDOUT
+if(args.length == 0){
+    System.err.println("Error: no input.");
+}
+else{
+    console.log(flipACoin(args.call));
 switch (args) {
     case 'call':
       console.log(flipACoin(args.call));
       break;
     default:
-        "Error: no input.";
+        console.log("Error: no input.");
 }
